a4f7c061684817cc0fd31490f988e6340126f07a
Chamal Hettiarachchi <chamalchamuditha1231@gmail.com>
2025-07-27 11:41:07 +0530

    Chamal | Adding composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ccwizma99/uoc-sports',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da6cf0dcefb492a6e6d232c9246a505040ca5406',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ccwizma99/uoc-sports' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da6cf0dcefb492a6e6d232c9246a505040ca5406',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
