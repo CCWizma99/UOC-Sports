f7d23abb5ff29738b4ab2d2d15039cdc56f59ee2
Jayashini <jayashinisjayaweera@gmail.com>
2025-09-02 10:41:38 +0530

    header.css

--- app/views/templates/equipment-manager/header.php
+++ app/views/templates/equipment-manager/header.php
@@ -1,20 +1,29 @@
-  <header>
-    <div class="header-container">      
-      <div class="logo-container">
-         <img src="/uoc-sports/public/images/uoc-logo.png" alt="Logo"  class="logo"><p>COLOMBO SPORTS<br>E-PORTAL</p>
-      </div>               
-      <h1>Equipment Management</h1>
-      <div>  
-        <a href="#">  
-      <button class="logout-btn"> <img src="uoc-sports/public/images/Logout.png" alt="logout-btn"  class="logout">Logout</button>
-        </a>  
-      </div>
+<header>
+    <link rel="stylesheet" type="text/css" href="header.css">
+  <div class="header-container">      
+    <div class="logo-container">
+       <img src="/uoc-sports/public/images/uoc-logo.png" alt="Logo" class="logo">
+       <p>COLOMBO SPORTS<br>E-PORTAL</p>
+    </div>               
+    <h1>Equipment Management</h1>
+    
+    <div class="logout-container"> 
+      <a href="#">  
+        <button class="logout-btn">
+          <img src="/uoc-sports/public/images/Logout.png" alt="logout-btn" class="logout">Logout
+        </button>
+      </a>  
     </div>
+  </div>
 
-    <div class="sub-nav">
-      <a href="http://localhost/uoc-sports/public/equipment-manager/" class="back-link">
-        <img src= "uoc-sports/public/images/back.png" alt="back" class="img">
-        Back</a>
-        <a href="/uoc-sports/public/equipment-manager/equipment-report" class="report-title">Report</a>
-    </div>
-  </header>
+  <div class="sub-nav">
+    <a href="http://localhost/uoc-sports/public/equipment-manager/" class="back-link">
+     <img src="/images/Logout.png" alt="back" class="img">
+       Back
+    </a>
+    
+    <a href="/uoc-sports/public/equipment-manager/equipment-report" class="report-title">
+      <span>Report</span>
+    </a>
+  </div>
+</header>
