81da4b9908e44a89e875369afbb70c31635df9fa
Jayashini <jayashinisjayaweera@gmail.com>
2025-10-13 09:37:48 +0530

    equipment

---
+++ app/views/equipment-manager/equipment.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Colombo Sports E-Portal</title>
+
+  <style>
+    @import url("/uoc-sports/public/css/equipment-manager/index.css");
+    @import url("/uoc-sports/public/css/equipment-manager/footer.css");
+    @import url("/upc-sports/public/css/equipment-manager/header.css");
+    @import url("/uoc-sports/public/css/equipment-manager/calendar.css");
+
+  </style>
+</head>
+
+<body>
+    <div class="Equipment">
+        <button class="btn">Report</button>
+        <h1>Equipment</h1>
+        <button class="btn">Add New Reservation +</button>
+    </div>
+</body>
+     
+</html>
