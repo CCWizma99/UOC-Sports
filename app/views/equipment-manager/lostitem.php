b2f5512c227aff3414d55b71698a614ea2e94c82
Jayashini <jayashinisjayaweera@gmail.com>
2025-10-20 16:34:00 +0530

    lost item display

--- app/views/equipment-manager/lostitem.php
+++ app/views/equipment-manager/lostitem.php
@@ -20,132 +20,95 @@
   
 ?>
 <body class="equipment-body">
-    <div class="btn">
-        <span class="report-btn"><a href="equipment-report.php">Report</a></span>
-        <span class="add-new"><a href="addequipment.php">Add New Equipment +</a></span>
-    </div>
+    
 
     <div class ="content">
 
     <div class="card">
-       <h3>Athletics - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Rugby- Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Tennis - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Weightlifting - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Basketball - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Carrom - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Scrable - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Chess - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Football - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Baseball - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Rowing - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Netball - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Taekwondo - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Hockey - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3>
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Swimming - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Elle - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
     <div class="card">
-       <h3>Cricket - Code</h3> 
+       <h3>Bottle</br> Found - Play ground </br> Code</h3> 
        <img src="#" class="image">     
     </div>
 
-    <div class="card">
-       <h3>Kabaddi - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Wrestling - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Badminton - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Table Tennis - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Volleyball - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Boxing - Code</h3> 
-       <img src="#" class="image">     
-    </div>
-
-    <div class="card">
-       <h3>Karate - Code</h3> 
-       <img src="#" class="image">     
-    </div>
 
 </div>
 
