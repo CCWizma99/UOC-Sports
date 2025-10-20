13a63840b0d3a92ed12116bbad46ff0547a14412
Jayashini <jayashinisjayaweera@gmail.com>
2025-10-20 08:51:42 +0530

    messages boxes sports manager

---
+++ app/views/sports-manager/message-records.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Message Boxes</title>
+  <link rel="stylesheet" href="message-records.css">
+</head>
+<body>
+
+
+<div class="chat-list">
+ <h2>Messages</h2>
+
+ 
+  <div class="chat-box" onclick="toggleMessages(this)">
+    <div class="chat-user">K S Silva</div>
+    <div class="chat-preview">Yes, I’ll be there by 10!</div>
+    <div class="chat-messages">
+      <div class="message sent">Hey Saman!</div>
+      <div class="message received">Hey, are you coming tomorrow?</div>
+      <div class="message sent">Yes, I’ll be there by 10!</div>
+    </div>
+  </div>
+
+
+  <div class="chat-box" onclick="toggleMessages(this)">
+    <div class="chat-user">N S Perera</div>
+    <div class="chat-preview">Hey Nimal, What is the time of the new practice session?</div>
+    <div class="chat-messages">
+      <div class="message received">Hi! I scheduled a new practice session.</div>
+      <div class="message sent">Hey Nimal, What is the time of the new practice session?</div>
+      
+      
+    </div>
+  </div>
+
+</div>
+
+<script>
+function toggleMessages(box) {
+  box.classList.toggle("active");
+}
+</script>
+
+</body>
+</html>
