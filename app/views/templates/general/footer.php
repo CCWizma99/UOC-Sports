de83712cd3a8e12333dee94b16901bd8094638f7
shashini-gif <shashinirajapaksha571@gmail.com>
2026-04-17 09:54:42 +0530

    Fix footer

--- app/views/templates/general/footer.php
+++ app/views/templates/general/footer.php
@@ -3,7 +3,8 @@
         <div class="footer-grid">
             <div class="footer-brand">
                 <h3>UOC Sports E-Portal</h3>
-                <p>Empowering athletic excellence and teamwork at the University of Colombo through innovative digital solutions.</p>
+                <p>Empowering athletic excellence and teamwork at the University of Colombo through innovative digital
+                    solutions.</p>
                 <div class="social-links">
                     <a href="#"><i class="fab fa-facebook-f"></i></a>
                     <a href="#"><i class="fab fa-linkedin-in"></i></a>
@@ -11,47 +12,38 @@
                     <a href="#"><i class="fab fa-youtube"></i></a>
                 </div>
             </div>
-            
+
             <div class="footer-section">
                 <h4>Platform</h4>
-                <a href="#">Home</a>
-                <a href="/uoc-sports/public/news">News & Events</a>
-                <a href="#">Facility Booking</a>
-                <a href="#">Training Programs</a>
+                <a href="/uoc-sports/public/">Home</a>
+                <a href="/uoc-sports/public/news">News &amp; Events</a>
+                <a href="/uoc-sports/public/facility-reservation">Facility Booking</a>
+                <a href="/uoc-sports/public/">Training Programs</a>
             </div>
-            
-            <div class="footer-section">
-                <h4>Sports</h4>
-                <a href="#">Cricket</a>
-                <a href="#">Rugby</a>
-                <a href="#">Athletics</a>
-                <a href="#">Karate</a>
-                <a href="#">Netball</a>
-            </div>
-            
+
             <div class="footer-section">
                 <h4>Resources</h4>
-                <a href="#">Student Portal</a>
-                <a href="#">Coaching Staff</a>
-                <a href="#">Facilities</a>
-                <a href="#">Championships</a>
+                <a href="/uoc-sports/public/student">Student Portal</a>
+                <a href="/uoc-sports/public/">Coaching Staff</a>
+                <a href="/uoc-sports/public/facility-reservation">Facilities</a>
+                <a href="/uoc-sports/public/results">Championships</a>
             </div>
-            
+
             <div class="footer-section">
                 <h4>Support</h4>
-                <a href="#">Help Center</a>
+                <a href="/uoc-sports/public/">Help Center</a>
                 <a href="/uoc-sports/public/#contact">Contact Us</a>
-                <a href="#">FAQs</a>
-                <a href="#">Feedback</a>
+                <a href="/uoc-sports/public/">FAQs</a>
+                <a href="/uoc-sports/public/">Feedback</a>
             </div>
         </div>
-        
+
         <div class="footer-bottom">
             <p>© 2025 University of Colombo, Sri Lanka. All rights reserved.</p>
             <div class="footer-links">
-                <a href="#">Privacy Policy</a>
-                <a href="#">Terms of Service</a>
-                <a href="#">Cookie Policy</a>
+                <a href="/uoc-sports/public/">Privacy Policy</a>
+                <a href="/uoc-sports/public/">Terms of Service</a>
+                <a href="/uoc-sports/public/">Cookie Policy</a>
             </div>
         </div>
     </div>
